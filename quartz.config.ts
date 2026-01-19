b04b023c477f7d328fa1881c73611f642e6b00a5
Lonnow <73619757+lonnow@users.noreply.github.com>
2026-01-19 19:32:00 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -77,7 +77,9 @@ const config: QuartzConfig = {
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
-      Plugin.ContentPage(),
+      Plugin.ContentPage({
+  enableLegacyExtension: true,
+}),
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
